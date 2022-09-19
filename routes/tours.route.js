7fb2d64155e90c3ed9d3bda0d67e13c58f662581
AbdurrahmanTalha <abdurrahmantalha331@gmail.com>
2022-09-19 22:25:54 +0600

    initial commit

---
+++ routes/tours.route.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const tourRouter = require("../controller/tour.controller");
+
+router.route("/")
+    .get(tourRouter.getTours)
+    .post(tourRouter.saveATour)
+
+
+router.route("/trending")
+    .get(tourRouter.getTrending)
+
+router.route("/:id")
+    .get(tourRouter.getTourById)
+    .patch(tourRouter.updateTourById)
+
+module.exports = router;
