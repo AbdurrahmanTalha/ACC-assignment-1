8ca5b02bb699eb5bee04dcbda5e4151fe8c82ee8
AbdurrahmanTalha <abdurrahmantalha331@gmail.com>
2022-09-20 09:00:12 +0600

    changes

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
 // server
 const port = process.env.PORT || 5000;
 
+
 app.listen(port, () => {
     console.log(`App is running on port ${port}`.yellow.bold);
 });
